5165084e49d55bab166eb73093e6edc1d06914a0
Maxmerock123 <Maxmerock@gmail.com>
2024-02-06 13:15:13 +0700

    add basic header nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,13 +8,16 @@ function App() {
 
   return (
     <>
-      <div className="flex justify-center">
-        <h1 className="text-3xl font-bold underline">Logo</h1>
-        <h3>About me</h3>
-        <h3>Exoeriences</h3>
-        <h3>Skills</h3>
-        <h3>Projects</h3>
-        <h3>Contact me</h3>
+      <div className="flex justify-center ">
+        <div className="flex w-[80%] items-center justify-between">
+          <h1 className="text-3xl font-bold underline">Logo</h1>
+          <h3>About me</h3>
+          <h3>Exoeriences</h3>
+          <h3>Skills</h3>
+          <h3>Projects</h3>
+          <h3>Contact me</h3>
+          <button className="px-5 py-2 bg-slate-400">button</button>
+        </div>
       </div>
     </>
   );
